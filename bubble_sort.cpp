1e8494354b9b2fe9f4df7fba798b0739342b87bb
ismailzia01 <140696113+ismailzia01@users.noreply.github.com>
2024-10-27 23:06:15 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -15,7 +15,7 @@ void bubble_sort(int a[], int len)
     int temp;
     for(int i = 0; i < len; i++)
     {
-        for(int j = 0; j < len-1; j++)
+        for(int j = 0; j < len - i -1; j++)
         {
             if(a[j] > a[j+1])
             {
