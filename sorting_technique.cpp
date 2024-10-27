56babf32b3f946355566b501cfd64a79d293046c
ismailzia01 <140696113+ismailzia01@users.noreply.github.com>
2024-10-27 23:05:43 +0530

    Update sorting_technique.cpp

--- sorting_technique.cpp
+++ sorting_technique.cpp
@@ -33,7 +33,7 @@ void Sort::bubble_sort(int a[], int len)
     int temp;
     for(int i = 0; i < len; i++)
     {
-        for(int j = 0; j <= len; j++)
+        for(int j = 0; j <= len -i -1; j++)
         {
             if(a[j] > a[j+1])
             {
