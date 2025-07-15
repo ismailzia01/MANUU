5e63e3257a985a292a1740a8ac199c4771857dd0
Mohammad Ismail Zia <140696113+ismailzia01@users.noreply.github.com>
2025-07-15 22:48:45 +0530

    Update StackLL.java

--- StackLL.java
+++ StackLL.java
@@ -13,7 +13,7 @@ public class StackLL {
 
     private Node top = null;
     private Scanner input = new Scanner(System.in);
-
+    //push an item in stack
     public void pushItem() {
         System.out.print("Enter item: ");
         int ele = input.nextInt();
